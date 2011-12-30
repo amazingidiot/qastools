fa486aba5a72fa2867ef1c7dedcf9cb0344159dd
Sebastian Holtermann <sebholt@xwmw.org>
2011-12-30 12:19:07 +0100

    License text in source files

---
+++ shared/src/license_texts.cpp
@@ -0,0 +1,31 @@
+//
+// C++ Implementation:
+//
+// Description:
+//
+//
+// Author: Sebastian Holtermann <sebholt@xwmw.org>, (C) 2011
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "license_texts.hpp"
+
+
+const char license_text_short[] = "\
+Copyright (C) 2011  Sebastian Holtermann <sebholt@xwmw.org>\n\
+\n\
+This program is free software: you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.\n\
+\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\
+";
---
+++ shared/src/license_texts.hpp
@@ -0,0 +1,18 @@
+//
+// C++ Interface:
+//
+// Description:
+//
+//
+// Author: Sebastian Holtermann <sebholt@xwmw.org>, (C) 2011
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef __INC_license_texts_hpp__
+#define __INC_license_texts_hpp__
+
+extern const char license_text_short[];
+
+#endif
