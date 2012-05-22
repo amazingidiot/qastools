96c03439a5e61c8d1a020b5e1cbb253a70a7822d
Sebastian Holtermann <sebholt@xwmw.org>
2012-05-22 12:49:57 +0200

    QSnd2::Proxies extended

--- shared/src/qsnd2/proxies.cpp
+++ shared/src/qsnd2/proxies.cpp
@@ -12,12 +12,21 @@ namespace QSnd2
 {
 
 
-Proxy::Proxy ( )
+Proxy::Proxy (
+	::QSnd2::Proxies_Group * group_n ) :
+_group ( group_n )
 {
 }
 
 Proxy::~Proxy ( )
 {
+}
+
+void
+Proxy::set_group (
+	::QSnd2::Proxies_Group * group_n )
+{
+	_group = group_n;
 }
 
 
@@ -90,6 +99,48 @@ List_Proxies_Group::List_Proxies_Group ( )
 }
 
 List_Proxies_Group::~List_Proxies_Group ( )
+{
+}
+
+
+Group1::Group1 ( ) :
+_sliders ( 0 ),
+_switches ( 0 ),
+_lists ( 0 )
+{
+}
+
+Group1::~Group1 ( )
+{
+}
+
+void
+Group1::set_sliders (
+	::QSnd2::Slider_Proxies_Group * grp_n )
+{
+	_sliders.reset ( grp_n );
+}
+
+void
+Group1::set_switches (
+	::QSnd2::Switch_Proxies_Group * grp_n )
+{
+	_switches.reset ( grp_n );
+}
+
+void
+Group1::set_lists (
+	::QSnd2::List_Proxies_Group * grp_n )
+{
+	_lists.reset ( grp_n );
+}
+
+
+Group2::Group2 ( )
+{
+}
+
+Group2::~Group2 ( )
 {
 }
 
--- shared/src/qsnd2/proxies.hpp
+++ shared/src/qsnd2/proxies.hpp
@@ -10,9 +10,14 @@
 #define __INC_qsnd_proxies_hpp__
 
 #include <QList>
+#include <QString>
+#include <QScopedPointer>
 
 namespace QSnd2
 {
+
+// Forward declaration
+class Proxies_Group;
 
 
 /// @brief Proxy
@@ -22,11 +27,33 @@ class Proxy
 	// Public methods
 	public:
 
-	Proxy ( );
+	Proxy (
+		::QSnd2::Proxies_Group * group_n = 0 );
 
 	virtual
 	~Proxy ( );
+
+	::QSnd2::Proxies_Group *
+	group ( ) const;
+
+	void
+	set_group (
+		::QSnd2::Proxies_Group * group_n );
+
+
+	// Private attributes
+	private:
+
+	::QSnd2::Proxies_Group * _group;
 };
+
+inline
+::QSnd2::Proxies_Group *
+Proxy::group ( ) const
+{
+	return _group;
+}
+
 
 /// @brief Proxies_Group
 ///
@@ -52,7 +79,7 @@ class Proxies_Group
 
 
 	// Private attributes
-	public:
+	private:
 
 	QList < ::QSnd2::Proxy * > _proxies;
 };
@@ -165,7 +192,15 @@ class List_Proxy :
 	List_Proxy ( );
 
 	~List_Proxy ( );
+};
 
+class List_Proxy_Item
+{
+	// Public methods
+	public:
+
+	QString name;
+	QString name_l10n;
 };
 
 /// @brief List_Proxies_Group
@@ -180,6 +215,9 @@ class List_Proxies_Group :
 
 	~List_Proxies_Group ( );
 
+	unsigned int
+	num_items ( ) const;
+
 	::QSnd2::List_Proxy *
 	proxy (
 		unsigned int idx_n ) const;
@@ -192,6 +230,116 @@ List_Proxies_Group::proxy (
 {
 	return static_cast < ::QSnd2::List_Proxy * >  (
 		::QSnd2::Proxies_Group::proxy ( idx_n ) );
+}
+
+
+/// @brief Group level 1 - holds a proxies list for every type
+///
+class Group1
+{
+	// Public methods
+	public:
+
+	Group1 ( );
+
+	virtual
+	~Group1 ( );
+
+
+	::QSnd2::Slider_Proxies_Group *
+	sliders ( ) const;
+
+	void
+	set_sliders (
+		::QSnd2::Slider_Proxies_Group * grp_n );
+
+
+	::QSnd2::Switch_Proxies_Group *
+	switches ( ) const;
+
+	void
+	set_switches (
+		::QSnd2::Switch_Proxies_Group * grp_n );
+
+
+	::QSnd2::List_Proxies_Group *
+	lists ( ) const;
+
+	void
+	set_lists (
+		::QSnd2::List_Proxies_Group * grp_n );
+
+
+	// Private attributes
+	private:
+
+	QScopedPointer < ::QSnd2::Slider_Proxies_Group > _sliders;
+	QScopedPointer < ::QSnd2::Switch_Proxies_Group > _switches;
+	QScopedPointer < ::QSnd2::List_Proxies_Group > _lists;
+};
+
+inline
+::QSnd2::Slider_Proxies_Group *
+Group1::sliders ( ) const
+{
+	return _sliders.data();
+}
+
+inline
+::QSnd2::Switch_Proxies_Group *
+Group1::switches ( ) const
+{
+	return _switches.data();
+}
+
+inline
+::QSnd2::List_Proxies_Group *
+Group1::lists ( ) const
+{
+	return _lists.data();
+}
+
+
+/// @brief Group level 2 - holds groups of level 1
+///
+class Group2
+{
+	// Public methods
+	public:
+
+	Group2 ( );
+
+	virtual
+	~Group2 ( );
+
+
+	unsigned int
+	num_groups ( ) const;
+
+	::QSnd2::Group1 *
+	group (
+		unsigned int idx_n );
+
+
+	// Private attributes
+	private:
+
+	QList < ::QSnd2::Group1 * > _groups;
+};
+
+inline
+unsigned int
+Group2::num_groups ( ) const
+{
+	return _groups.size();
+}
+
+inline
+::QSnd2::Group1 *
+Group2::group (
+	unsigned int idx_n )
+{
+	return _groups[idx_n];
 }
 
 
