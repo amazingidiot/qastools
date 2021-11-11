a055462e23cb16abe880ee7adf91a4a88b950a69
Oliver Sahr <oli.sahr@gmail.com>
2021-11-11 21:32:42 +0100

    some files were modified for some reason

---
+++ qashctl/src/qastools_config.hpp
@@ -0,0 +1,27 @@
+
+#ifndef __INC_qas_config_hpp__
+#define __INC_qas_config_hpp__
+
+#define PACKAGE_NAME "qastools"
+#define PACKAGE_TITLE "QasTools"
+#define PACKAGE_VERSION "0.24.0"
+
+#define PROGRAM_NAME "qashctl"
+#define PROGRAM_TITLE "QasHctl"
+#define VERSION "0.24.0"
+#define L10N_PREFIX "qastools_"
+
+#define SINGLE_APPLICATION_KEY "qashctl-0.24.0"
+
+#define INSTALL_DIR_DATA "/usr/local/share/qastools"
+#define INSTALL_DIR_L10N "/usr/local/share/qastools/l10n"
+#define INSTALL_DIR_APP_ICONS "/usr/local/share/qastools/icons"
+#define INSTALL_DIR_WIDGETS_GRAPHICS "/usr/local/share/qastools/widgets"
+
+#define INSTALL_DIR_ICONS_PNG_16 "/usr/local/share/icons/hicolor/16x16/apps"
+#define INSTALL_DIR_ICONS_PNG_32 "/usr/local/share/icons/hicolor/32x32/apps"
+#define INSTALL_DIR_ICONS_PNG_48 "/usr/local/share/icons/hicolor/48x48/apps"
+#define INSTALL_DIR_ICONS_PNG_64 "/usr/local/share/icons/hicolor/64x64/apps"
+#define INSTALL_DIR_ICONS_SVG "/usr/local/share/icons/hicolor/scalable/apps"
+
+#endif
